ee8e7b8689fb57e883ef6951b4a5cd5f1b64b5b2
Jacob Dury <jacobdury2007@gmail.com>
2026-04-21 10:56:48 -0400

    ajout de 12B

--- app/src/main/java/com/example/annexe12/MainActivity.java
+++ app/src/main/java/com/example/annexe12/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.annexe12;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +17,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    Ecouteur ec;
+    TextView champReponse;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Je crée mon singleton
+        GestionBD instance = GestionBD.getInstance(getApplicationContext());
+        System.out.println(instance.retournerInventions().size());
+
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> apapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, instance.retournerInventions());
+        listView.setAdapter(apapter);
+
+        champReponse = findViewById(R.id.reponse);
+
+        ec = new Ecouteur();
+        listView.setOnItemClickListener(ec);
+    }
+    public class Ecouteur implements AdapterView.OnItemClickListener {
+
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            if(GestionBD.getInstance(getApplicationContext()).retournerSiMatch("Mary Anderson", listView.getItemAtPosition(position).toString()))
+                champReponse.setText("Bonne réponse");
+            else{
+                champReponse.setText("Mauvaise réponse");
+                champReponse.setBackgroundColor(Color.RED);
+            }
+
+        }
     }
 }
